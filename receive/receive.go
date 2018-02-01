ef278bdb4daaa8ba3bdaf94ad7b80cd1e14f70e2
ttexan <ttexan.developper@gmail.com>
2018-02-01 12:18:04 +0900

    modify after presentation

--- receive/receive.go
+++ receive/receive.go
@@ -13,8 +13,6 @@ type Data struct {
 	Point int    `json:"point"`
 }
 
-var recentData Data
-
 func SendDataHandler(w http.ResponseWriter, r *http.Request) {
 	var data Data
 	err := json.NewDecoder(r.Body).Decode(&data)
@@ -22,26 +20,13 @@ func SendDataHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	recentData = data
 	fmt.Printf("%#v\n", data)
 	w.WriteHeader(http.StatusCreated)
-}
-
-func saveDataHandler(w http.ResponseWriter, r *http.Request) {
-	ping := recentData
-	res, err := json.Marshal(ping)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/api/v1/scores", http.HandlerFunc(SendDataHandler))
-	mux.Handle("/api/v1/score", http.HandlerFunc(saveDataHandler))
 	handler := cors.AllowAll().Handler(mux)
 	http.ListenAndServe(":8080", handler)
 }
